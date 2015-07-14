2fd503d920bb7b14c8eff6a80ba0f38c0e5ca3b7
Roger CLANGET <rclanget@e1r2p16.42.fr>
2015-07-14 21:00:20 +0200

    maj

--- Framework/urls.py
+++ Framework/urls.py
@@ -2,10 +2,6 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'Framework.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
     url(r'', include('intra.urls')),
     url(r'^i18n/', include('django.conf.urls.i18n')),
     url(r'^forum/', include('forum.urls')),
